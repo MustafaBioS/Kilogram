60e1728dfac6aec2bc7f78534f37a7e237d53869
MustafaBioS <bioshpere.voices@gmail.com>
2025-11-07 02:38:27 +0200

    ADD - Account page base

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_bcrypt import Bcrypt
-from flask_login import UserMixin
+from flask_login import UserMixin, current_user, LoginManager
 from dotenv import load_dotenv
 import os
 
@@ -21,17 +21,32 @@ migrate = Migrate(app, db)
 
 bcrypt = Bcrypt(app)
 
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login"
+
 # Database
+
+class Users(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), nullable=False, unique=False)
+    password = db.Column(db.String(64), nullable=False)
 
 
 
 # Routes
 
+@login_manager.user_loader
+def load_user(user_id):
+    return Users.query.get(int(user_id))
+
 @app.route("/")
 def home():
     return render_template("index.html")
 
-
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    return render_template("account.html")
 
 # Run
 
